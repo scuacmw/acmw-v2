544db2865637c91ebae67c752b434c508b5fe973
plandry240z <159398849+plandry240z@users.noreply.github.com>
2026-05-26 12:39:01 -0700

    updated acm-w board and cleaned up photos

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './Board.css';
-import image6 from '../assets/maddie.jpg'; //President
-import image7 from '../assets/arya.jpg'; //Vice President
-import image8 from '../assets/Gina_Chiu.jpg'; //PR and Media
-import image9 from '../assets/sanjna.jpg'; //hackathon coordinator
-import image10 from '../assets/sara.jpg'; //co-director of events
-import image11 from '../assets/Anna_Aldrin.png'; //co director of events
-import image12 from '../assets/kaavya.jpg'; // web master
-import image13 from '../assets/daniela.jpg'; // cybersisters event director
-import image14 from '../assets/tanvi.jpg'; //college and career advisor
-import image15 from '../assets/priyanka.jpg'; //secretary
-import image16 from '../assets/cath.jpg'; // treasurer
-import image18 from '../assets/paulina.jpg'; //IL 1
-import image19 from '../assets/emaan.jpg'; //IL 2
+import prez_acmw from '../assets/acmwBoard/arya.jpg'; //President
+import vp_acmw from '../assets/acmwBoard/sara.jpg'; //Vice President
+import pr_acmw from '../assets/acmwBoard/leslie.jpeg'; //Director of Media and PR
+import hc_acmw from '../assets/acmwBoard/kaavya.jpg'; //hackathon coordinator
+import tc_acmw from '../assets/acmwBoard/tanvi.jpg'; //technical events coordinator
+import web_acmw from '../assets/acmwBoard/paulina.jpg'; // web master
+import soc_acmw from '../assets/acmwBoard/daniela.jpg'; // social events coordinator
+import con_acmw from '../assets/acmwBoard/faith.jpeg'; //conference and career strategist
+import sec_acmw from '../assets/acmwBoard/shrim.jpeg'; //secretary
+import tres_acmw from '../assets/acmwBoard/priyanjal.png'; // treasurer
+import il1_acmw from '../assets/acmwBoard/anvita.png'; //IL 1
+import il2_acmw from '../assets/acmwBoard/Priyanka_Burra.jpg'; //IL 2
 import image20 from '../assets/meettheboardbanner.png';
-import image21 from '../assets/Laila_Heigre.jpg';
-import image22 from '../assets/Priyanka_Burra.jpg';
 import prez_acm from '../assets/acmBoard/chris.jpg';
 import vp_acm from '../assets/acmBoard/rayyan.jpg';
 import wc_acm from '../assets/acmBoard/espy.jpg';
@@ -29,27 +26,24 @@ import sec_acm from '../assets/acmBoard/mariam.jpg';
 import hc_acm from '../assets/acmBoard/ahbi.jpg';
 
 const boardMembers = [
-  { name: 'Maddie Follosco', role: 'President', image: image6 },
-  { name: 'Arya Deshpande', role: 'Vice President', image: image7 },
-  { name: 'Gina Chiu', role: 'Director of Media and PR', image: image8 },
-  { name: 'Priyanka Ganguly', role: 'Secretary', image: image15 },
-  { name: 'Kaavya Jethwa', role: 'Web-Master', image: image12 },
-  { name: 'Anna Aldrin', role: 'Director of Events', image: image11 },
-  { name: 'Sara Donnelly', role: 'Events Coordinator', image: image10 },
-  { name: 'Catherine Hung', role: 'Treasurer', image: image16 },
-  { name: 'Tanvi Gandhi', role: 'Career and Conference Advisor', image: image14 },
-  { name: 'Sanjna Sood', role: 'Hack-a-thon Coordinator', image: image9 },
-  { name: 'Emaan Zaidi', role: 'Industry Liaison', image: image19 },
-  { name: 'Daniela Casillas', role: 'Cybersisters Coordinator', image: image13 },
-  { name: 'Paulina Landry', role: 'Industry Liaison', image: image18 },
-  { name: 'Priyanka Burra', role: "Freshman Representative", image: image22 },
-  { name: 'Laila Heigre', role: "Freshman Representative", image: image21 }
+  { name: 'Arya Deshpande', role: 'President', image: prez_acmw },
+  { name: 'Sara Donnelley', role: 'Vice President', image: vp_acmw },
+  { name: 'Shrimayi Sinkar babu Bharathy', role: 'Secretary', image: sec_acmw },
+  { name: 'Priyanjal Poswal', role: 'Treasurer', image: tres_acmw },
+  { name: 'Paulina Landry', role: 'Web-Master', image: web_acmw },
+  { name: 'Leslie Mejia', role: 'Director of Media and PR', image: pr_acmw },
+  { name: 'Tanvi Gandhi', role: 'Technical Events Coordinator', image: tc_acmw },
+  { name: 'Daniela Casillas', role: 'Social Events Coordinator', image: soc_acmw },
+  { name: 'Faith Ogunfunmi', role: 'Conference/Career Strategist', image: con_acmw },
+  { name: 'Kaavya Jethwa', role: 'Hack-a-thon Coordinator', image: hc_acmw },
+  { name: 'Anvita Buddhavaram', role: 'Industry Liaison', image: il1_acmw },
+  { name: 'Priyanka Burra', role: 'Industry Liaison', image: il2_acmw },
 ];
 
 // Placeholder section: add your imports above (e.g. import imageXX from '../assets/Name.jpg') and replace null with the variable
 const PLACEHOLDER_IMAGE = 'https://via.placeholder.com/220';
 const boardMembersSecondary = [
-  { name: 'Chris Shobe', role: 'President', image: prez_acm },
+  { name: 'Chris Shobe', role: 'anPresident', image: prez_acm },
   { name: 'Rayyan Hussain', role: 'Vice President', image: vp_acm },
   { name: 'Esperanza Aguilar', role: 'Workshop Coordinator', image: wc_acm },
   { name: 'Neelasha Sudarshan', role: 'Webmaster', image: web_acm },
